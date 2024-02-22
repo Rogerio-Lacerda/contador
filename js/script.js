043710db79bb037961908387c73f518396b626e0
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2024-02-22 15:13:54 -0300

    Alterei a data do ano atual no script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-import Contador from "./contador.js";
+import Contador from './contador.js';
 
-const contador = new Contador("24 December 2023 23:59:59 GMT-0300");
+const contador = new Contador('24 December 2024 23:59:59 GMT-0300');
 
-const contadorTexto = document.querySelector(".grid-contador");
+const contadorTexto = document.querySelector('.grid-contador');
 
 contadorTexto.innerHTML = `
   <p class="contador">${contador.faltamDias.days}<span>Dias</span></p>
